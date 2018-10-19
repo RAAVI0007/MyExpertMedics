58c2308f4056b16fd57e2aec4d80a9cffbc5d0e6
RAAVI007 <ravi.devireddy07@gmail.com>
2018-10-19 23:10:03 +0530

    Stater

---
+++ EclipseWorkSpace/ExpertMedics/src/main/java/com/expertmedics/ExpertmedicsApplication.java
@@ -0,0 +1,11 @@
+package com.expertmedics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertmedicsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertmedicsApplication.class, args);
+	}
+}
