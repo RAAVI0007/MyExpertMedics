faf3b62c2ff61588aad080816538df8061750d4c
DeviReddy Ravi <39902353+RAAVI0007@users.noreply.github.com>
2018-10-27 01:32:23 +0530

    Appointment-Module
    Office

--- appointment.component.ts
+++ appointment.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { TestServiceService } from '../../services/test-service.service';
 
 @Component({
   selector: 'app-appointment',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentComponent implements OnInit {
 
-  constructor() { }
-
+  title = 'My Exeprt Medics';
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+  posts: any;
+  onSubmit() {
+    alert ('seding data') ;
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    // Initialize Params Object
+    let Params = new HttpParams();
+    // Begin assigning parameters
+    Params = Params.append('firstParameter', this.registerForm.value.firstName);
+    Params = Params.append('secondParameter', this.registerForm.value.lastName);
+    alert (Params) ;
+   /* return this.http.post('http://localhost:8080/RESTWebService/rest/lvcalc/rsfsjson', {params: { params: Params }}).subscribe(data => {
+        this.posts = data; 
+        console.log(this.posts);
+      });  */
+
+ 
+  return this.http.post('http://localhost:8080/RESTWebService/rest/lvcalc/rsfsjson', "Hello")
+                  .toPromise()
+                  .then((data) => data);
+
   }
 
 }
